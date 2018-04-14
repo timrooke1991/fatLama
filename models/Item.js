6e432234883092cd77f2359e0477db021346f397
timrooke1991 <timrooke1991@gmail.com>
2018-04-14 19:27:46 +0100

    Weighting adjustment

--- models/Item.js
+++ models/Item.js
@@ -24,7 +24,7 @@ function calculateTextSimilarity(userText, itemName) {
 
 function calculateRelevance(distance, similarity, factor) {
   
-  const weight = 2 || factor;
+  const weight = 3 || factor;
 
   // Inverse distance; required to make it comparable to text
   const normalisedDistance = 1 - distance;
